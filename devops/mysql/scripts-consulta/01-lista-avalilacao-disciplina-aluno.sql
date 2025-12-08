e75209f8feb1b314078ccd471ca48bd864ec5fab
Felipe J. R. Vieira <felipejrvieira@gmail.com>
2025-12-08 16:34:16 -0300

    Implementação de sistema multiagente com seguinte Fluxo: Agente de Comunicação → Agente de Avaliação → Agente de Tutoria

--- devops/mysql/scripts-consulta/01-lista-avalilacao-disciplina-aluno.sql
+++ devops/mysql/scripts-consulta/01-lista-avalilacao-disciplina-aluno.sql
@@ -1,5 +1,13 @@
-SELECT d.Nome, a.Tipo, a.Peso, a.ValorNota, al.Nome, al.Sobrenome, m.Status, m.NotaFinal, m.Frequencia from Avaliacao a
+SELECT concat(al.Nome," ",al.Sobrenome) as name, 
+ d.Nome as discipline,
+ a.Tipo, 
+ a.Peso, 
+ a.ValorNota,
+ m.Frequencia
+from Avaliacao a	
 INNER JOIN Matricula m on a.ID_Matricula = m.ID_Matricula
-INNER JOIN Aluno al on al.ID_Aluno = al.ID_Aluno
+INNER JOIN Aluno al on al.ID_Aluno = m.ID_Aluno
 INNER JOIN Turma t on t.ID_Turma = m.ID_Turma
 INNER JOIN Disciplina d on d.ID_Disciplina= t.ID_Disciplina
+WHERE al.ID_Aluno = 6-- and d.ID_Disciplina = 1
+order by t.ID_Turma
